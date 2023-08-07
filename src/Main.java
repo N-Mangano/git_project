e3d7066467eb2e99b1b43969d9481a079ff21239
Nadezda Mangano <nadenka24@yandex.ru>
2023-08-07 14:43:54 +0200

    fix: change world to name

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello Nadja!");
+    System.out.println("Hello Nadia!");
     System.out.println("Bye!");
   }
 }
