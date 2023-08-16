481c2f65bd92f915c4a557d65c2fb227e7221bbe
Nadezda Mangano <nadenka24@yandex.ru>
2023-08-16 20:07:16 +0200

    fix: optimize task1

--- src/Algorythm/Task1.java
+++ src/Algorythm/Task1.java
@@ -7,14 +7,10 @@ public class Task1 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int size = scanner.nextInt();
-    scanner.nextLine();
-    int[] array = new int[size];
-    for (int i = 0; i < size; i++) {
-      array[i] = scanner.nextInt();
-    }
     int sum = 0;
-    for (int i = 0; i < array.length; i++) {
-      sum += array[i];
+    for (int i = 0; i < size; i++) {
+      int x = scanner.nextInt();
+      sum += x;
     }
     System.out.println(sum);
   }
